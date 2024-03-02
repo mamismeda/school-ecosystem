8e1a4ba064f7146259a116510b33bb7b499e779a
mamismeda <amamismeda@gmail.com>
2024-03-02 17:20:04 +0400

    creating roots

--- private/controllers/home.php
+++ private/controllers/home.php
@@ -5,7 +5,6 @@ class Home extends Controller
 {
     function __construct()
     {
-        
+        echo "this is the home controller ";
     }
 }
-echo "this is the home controller ";
---
+++ private/controllers/students.php
@@ -0,0 +1,10 @@
+<?php
+
+// home controller 
+class Students extends Controller
+{
+    function __construct()
+    {
+        echo "this is the students controller ";
+    }
+}
