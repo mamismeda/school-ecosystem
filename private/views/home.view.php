93a611eb62369ab92818817e71f68d6ff29736d0
mamismeda <amamismeda@gmail.com>
2024-03-02 19:23:57 +0400

    refactoring code to avoid bugs

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php $this-> view('includes/header')?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="../public/assets/bootstrap.min.css">
-    <link rel="stylesheet" href="../public/assets/all.min.css">
-</head>
+<div class="container-fluid">
+    <h1> <i class="fa fa-plus"></i>This is home</h1>
+</div>
 
-<body>
-    <div class="container-fluid">
-        <h1> <i class="fa fa-plus"></i>This is home</h1>
-    </div>
-</body>
+<?php $this-> view('includes/footer')?>
 
-</html>
+
