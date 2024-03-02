16dae7815505498af50bf9e5850fcf0ffbf9b275
mamismeda <amamismeda@gmail.com>
2024-03-02 16:38:09 +0400

    creating app component

--- private/core/app.php
+++ private/core/app.php
@@ -1,3 +1,19 @@
 <?php
 
-echo 'from app.php';
+// main app file
+class App
+{
+    protected $controller = "home";
+    protected $method = "index";
+    protected $params = array();
+    public function __construct()
+    {
+        print_r($this->getURL());
+    }
+
+    private function getURL()
+    {
+        echo "<pre>";
+        print_r($_GET);
+    }
+}
