813ffc8558db42ffb047f4bf39be1416c0d8f5cf
mamismeda <amamismeda@gmail.com>
2024-03-03 18:23:00 +0400

    added assets

--- private/views/login.view.php
+++ private/views/login.view.php
@@ -1,8 +1,15 @@
 <?php $this->view('includes/header') ?>
 
 <div class="container-fluid">
-    <div class="shadow rounded m-auto" style="width: 100%;max-width: 310px">
-        something to see
+    <div class="p-3 shadow rounded mx-auto" style="margin-top:50px;width: 100%;max-width: 340px">
+        <h2>My School</h2>
+        <img src="<?=ROOT?>/logo" alt="">
+        <h3>Login</h3>
+        <input class="form-control" type="email" name="email" placeholder="Email" autofocus>
+        <br>
+        <input class="form-control" type="password" name="password" placeholder="Password">
+        <br>
+        <button class="btn btn-primary">Login</button>
     </div>
 </div>
 
