f1925985bca96bf87497f0b76bdd071f2e1cdeee
mamismeda <amamismeda@gmail.com>
2024-03-06 06:11:02 +0400

    created searching bar and made links clickable

--- private/views/includes/nav.view.php
+++ private/views/includes/nav.view.php
@@ -1,12 +1,12 @@
 <style>
   nav ul li a:hover {
     background-color: #ccc;
-    color:#fff !important;
+    color: #fff !important;
   }
 </style>
 <nav class="navbar navbar-expand-lg bg-body-tertiary p-2">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="<?= ROOT ?>">
       <img src="<?= ROOT ?>/assets/logo.png" style="width:50px"> My School </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -14,23 +14,26 @@
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+          <a class="nav-link active" aria-current="page" href="<?= ROOT ?>">Dashboard</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Classes</a>
+          <a class="nav-link" href="<?= ROOT ?>">Users</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Tests</a>
+          <a class="nav-link" href="<?= ROOT ?>">Classes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?= ROOT ?>/tests">Tests</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             USER
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><a class="dropdown-item" href="#">Dashboard</a></li>
+            <li><a class="dropdown-item" href="<?= ROOT ?>/profile">Profile</a></li>
+            <li><a class="dropdown-item" href="<?= ROOT ?>">Dashboard</a></li>
             <div class="dropdown-divider"></div>
-            <li><a class="dropdown-item" href="#">Logout</a></li>
+            <li><a class="dropdown-item" href="<?= ROOT ?>/logout">Logout</a></li>
           </ul>
         </li>
       </ul>
--- private/views/profile.view.php
+++ private/views/profile.view.php
@@ -4,10 +4,11 @@
 <div class="container-fluid p-4 shadow mx-auto" style="max-width:1000px">
     <?php $this->view('includes/crumbs') ?>
     <div class="row">
-        <div class="col-4">
+        <div class="col-sm-4 col-md-3">
             <img src="<?= ASSETS ?>/user_female.jpg" alt="" class="border border-primary d-block max-auto rounded-circle" style="width:150px;">
+            <h3 class="text-center">Mary Phiri</h3>
         </div>
-        <div class="col-8 bg-light p-2">
+        <div class="col-sm-8 col-md-9 bg-light p-2">
             <table class="table table-hover table-striped table-bordered">
                 <tr>
                     <th>First Name:</th>
@@ -21,11 +22,34 @@
                     <th>Gender:</th>
                     <td>Female</td>
                 </tr>
+                <tr>
+                    <th>Date Created:</th>
+                    <td>2024-08-02</td>
+                </tr>
             </table>
         </div>
     </div>
-    <div>
-        
+    <hr>
+    <div class="container-fluid">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Basic Info</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Classes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tests</a>
+            </li>
+        </ul>
+        <nav class="navbar bg-body-tertiary">
+            <form class="container-fluid">
+                <div class="input-group">
+                    <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-magnifying-glass"></i>&nbsp</span>
+                    <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1">
+                </div>
+            </form>
+        </nav>
     </div>
 </div>
 
