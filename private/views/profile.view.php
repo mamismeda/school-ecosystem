70ef96dcd1c56aa6eb64fc986b004629ccc2cf62
mamismeda <amamismeda@gmail.com>
2024-03-06 05:39:10 +0400

    added crumbs for ease rout navigation

--- private/views/profile.view.php
+++ private/views/profile.view.php
@@ -1,8 +1,9 @@
-<?php $this-> view('includes/header')?>
-<?php $this-> view('includes/nav')?>
+<?php $this->view('includes/header') ?>
+<?php $this->view('includes/nav') ?>
 
-<div class="container-fluid">
-    <h1> <i class="fa fa-plus"></i>This is home</h1>
+<div class="container-fluid p-4 shadow mx-auto" style="max-width:1000px">
+    <?php $this->view('includes/crumbs') ?>
+    <h4>Profile</h4>
 </div>
 
-<?php $this-> view('includes/footer')?>
+<?php $this->view('includes/footer') ?>
