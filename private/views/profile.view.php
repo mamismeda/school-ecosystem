5bde06ab9edc9b56a5be404a09346627401bb2db
mamismeda <amamismeda@gmail.com>
2024-03-06 05:48:16 +0400

    added table on prifle page

--- private/views/profile.view.php
+++ private/views/profile.view.php
@@ -3,7 +3,30 @@
 
 <div class="container-fluid p-4 shadow mx-auto" style="max-width:1000px">
     <?php $this->view('includes/crumbs') ?>
-    <h4>Profile</h4>
+    <div class="row">
+        <div class="col-4">
+            <img src="<?= ASSETS ?>/user_female.jpg" alt="" class="border border-primary d-block max-auto rounded-circle" style="width:150px;">
+        </div>
+        <div class="col-8 bg-light p-2">
+            <table class="table table-hover table-striped table-bordered">
+                <tr>
+                    <th>First Name:</th>
+                    <td>Mary Phiri</td>
+                </tr>
+                <tr>
+                    <th>Last Name:</th>
+                    <td>Phiri</td>
+                </tr>
+                <tr>
+                    <th>Gender:</th>
+                    <td>Female</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div>
+        
+    </div>
 </div>
 
 <?php $this->view('includes/footer') ?>
