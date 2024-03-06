728d8d3487be176918a2c472ccb320096797452b
mamismeda <amamismeda@gmail.com>
2024-03-06 07:03:55 +0400

    creating database connection

--- private/core/database.php
+++ private/core/database.php
@@ -1,2 +1,10 @@
 <?php
 
+// Database connection
+class Database {
+    private function connect()
+    {
+        $string = "mysql:host=localhost;dbname=school_db";
+        $con = new PDO($string, 'root', '');
+    }
+}
